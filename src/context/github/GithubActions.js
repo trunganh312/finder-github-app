0353517ddc1b76713ae88eed20cfef5d2bcf60dc
Trung Anh <tieupink312@gmail.com>
2022-06-14 16:20:15 +0700

    update

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_URL = 'https://api.github.com'
+const GITHUB_TOKEN = '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'
 
 const github = axios.create({
   baseURL: GITHUB_URL,
